8bd44161abdb4fbb24e0d1833a75dd32a5c456e4
tomasz.bajorek <tomasz.bajorek@orba.co>
2020-04-30 10:32:25 +0200

    remove unnecessary tests

--- Model/Config/ConfigRepository.php
+++ Model/Config/ConfigRepository.php
@@ -6,10 +6,16 @@
 namespace Orba\Config\Model\Config;
 
 use Exception;
+use Magento\Framework\Exception\LocalizedException;
 use Orba\Config\Model\Config;
 use Orba\Config\Model\ResourceModel\Config\CollectionFactory;
 use Orba\Config\Model\ResourceModel\Config as ConfigResourceModel;
 
+/**
+ * Class ConfigRepository
+ * @package Orba\Config\Model\Config
+ * @codeCoverageIgnore
+ */
 class ConfigRepository
 {
     /** @var CollectionFactory */
@@ -45,6 +51,11 @@ class ConfigRepository
     public function updateConfigs(array $configs): void
     {
         foreach ($configs as $config) {
+            if (!($config instanceof Config)) {
+                throw new LocalizedException(
+                    __('Wrong config object was provided to repository for update')
+                );
+            }
             $config->save();
         }
     }
